07de7fc0489c17d3dbdb96c2040d8a6718d1dec6
RivaD2 <rivkadavidowski@gmail.com>
2020-10-26 20:18:04 -0700

    changed arrow function to async in user schema

--- src/auth/models/user/user-schema.js
+++ src/auth/models/user/user-schema.js
@@ -1,25 +1,25 @@
 'use strict'
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
 //Create a schema structure
 const UsersSchema = new mongoose.Schema({
     // When this is stored, each obj in array will be key value pair
         // The key will be the index and the value will be the string
-        _id: mongoose.Schema.Types.ObjectId,
         username: { type: String, unique : true },
         password: { type: String }
     });
-
-    UsersSchema.pre('save', (next) => {
+//built in mongo method of pre
+    UsersSchema.pre('save', async function (next) {
         var user = this;
 
         // only hash the password if it has been modified (or is new)
         if (!user.isModified('password')) return next();
 
         // generate a salt
-        bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        bcrypt.genSalt(proces.env.SALT_WORK_FACTOR, (err, salt) => {
             if (err) return next(err);
 
             // hash the password along with our new salt
