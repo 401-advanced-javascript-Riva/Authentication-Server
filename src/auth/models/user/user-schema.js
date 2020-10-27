633886860a8789d19ff106d710e99576b9e477f0
RivaD2 <rivkadavidowski@gmail.com>
2020-10-26 20:27:56 -0700

    edited env file

--- src/auth/models/user/user-schema.js
+++ src/auth/models/user/user-schema.js
@@ -12,24 +12,19 @@ const UsersSchema = new mongoose.Schema({
         password: { type: String }
     });
 //built in mongo method of pre
-    UsersSchema.pre('save', async function (next) {
-        var user = this;
+UsersSchema.pre('save', async function (next) {
+    var user = this;
 
-        // only hash the password if it has been modified (or is new)
-        if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-        // generate a salt
-        bcrypt.genSalt(proces.env.SALT_WORK_FACTOR, (err, salt) => {
-            if (err) return next(err);
+    // generate a salt
+    const salt = await bcrypt.genSalt(SALT);
+    // hash the password along with our new salt
+    const hash = bcrypt.hash(user.password, salt)
+    // override the cleartext password with the hashed one
+    user.password = hash;
+    next();
+});
 
-            // hash the password along with our new salt
-            bcrypt.hash(user.password, salt, (err, hash)  => {
-                if (err) return next(err);
-
-                // override the cleartext password with the hashed one
-                user.password = hash;
-                next();
-            });
-        });
-    });
  module.exports = UsersSchema;
