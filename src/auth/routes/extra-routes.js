3383b1c6c47ce381a1d441759d32286270f3a84e
RivaD2 <rivkadavidowski@gmail.com>
2020-10-27 20:27:09 -0700

    res.json user in secret route

--- src/auth/routes/extra-routes.js
+++ src/auth/routes/extra-routes.js
@@ -8,9 +8,8 @@ const asyncWrapper = require('../../middleware/asyncWrapper');
 
 router.get('/secret', bearerAuth, asyncWrapper(async(req,res, next) => {
     console.log('getting route secret')
-    await res.status(200).json({
-        messages: "all messages"
-    })
+    //bearer auth assigns user to the response object
+    await res.status(200).json(res.user);
 }));
 
 module.exports = router;
--- src/middleware/bearer.js
+++ src/middleware/bearer.js
@@ -4,9 +4,11 @@ const Users = require('../auth/models/user/user-model');
 
 module.exports = async function bearerAuth(req, res, next) {
     console.log('bearerAuth');
-    //get token that is sent, verify it is the correct user and then return user in function in post
-    //token comes from the header
-    //format is bearer, followed by token
+    /*
+     - get token that is sent, verify it is the correct user and then return user in function in post
+     - token comes from the header
+     - format is bearer, followed by token
+     */
     const authHeader = req.headers['authorization'];
     console.log('authHeader', authHeader);
     //space between bearer and token so I have to split it
@@ -17,12 +19,14 @@ module.exports = async function bearerAuth(req, res, next) {
         try {
             const token = await Users.validateToken(jwtToken);
             console.log('is this the token', token);
-            //verify token by passing in token and secret
+            //username is imbedded in token, so I can see username in token
+            //I need to use this token to fetch user data
+            const result = await Users.find({ username: token.name })
+            res.user = result;
             next();
         } catch {
             //the value is serialized, and 403 tells the user they do not have a valid token
             return res.sendStatus(403);
-
         }
     } else {
         res.sendStatus(401);
